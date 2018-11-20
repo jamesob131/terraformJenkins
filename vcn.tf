d4f29ce738fe6274d0aa2bf65a9efab67ef9b63f
James O'Boyle <oboyle.james13@gmail.com>
2018-11-20 16:36:27 -0500

    Update vcn.tf

--- vcn.tf
+++ vcn.tf
@@ -9,14 +9,6 @@ variable "AD2" {default = "FsFj:US-ASHBURN-AD-2"}
 # ------ Create a new VCN
 variable "VCN-CIDR" { default = "10.0.0.0/16" }
 
-provider "oci" {
- tenancy_ocid = "${var.tenancy_ocid}"
- user_ocid = "${var.user_ocid}"
- fingerprint = "${var.fingerprint}"
- private_key_path = "${var.private_key_path}"
- region = "us-ashburn-1"
-}
-
 resource "oci_core_virtual_network" "vcn" {
  cidr_block = "10.0.0.0/16"
  dns_label = "tfvcn"
