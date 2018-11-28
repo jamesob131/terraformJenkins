f784fe27e6f1163852a8f3748d41e8a929c6449b
James O'Boyle <oboyle.james13@gmail.com>
2018-11-28 14:16:39 -0500

    remove vcn

--- vcn.tf
+++ vcn.tf
@@ -9,10 +9,3 @@ provider "oci" {
   auth = "InstancePrincipal"
   region = "us-ashburn-1"
 }
-
-resource "oci_core_virtual_network" "VCN" {
-  cidr_block     = "10.0.0.0/16"
-  dns_label      = "VCN"
-  compartment_id = "${var.compartment_ocid}"
-  display_name   = "VCN"
-}
