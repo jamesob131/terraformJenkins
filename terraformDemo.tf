5299b0a997450270e55ef17ee96618c404a6c595
James O'Boyle <oboyle.james13@gmail.com>
2019-01-22 14:11:58 -0500

    no change

--- terraformDemo.tf
+++ terraformDemo.tf
@@ -1,3 +1,15 @@
+variable "tenancy_ocid" {}
+variable "user_ocid" {}
+variable "fingerprint" {}
+variable "private_key_path" {}
+variable "compartment_ocid" {}
+variable "region" {}
+
+provider "oci" {
+  auth   = "InstancePrincipal"
+  region = "us-ashburn-1"
+}
+
 resource "oci_core_virtual_network" "VCN" {
   cidr_block     = "10.0.0.0/16"
   dns_label      = "VCN"
